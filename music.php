9a743f19b490b09af264b350b15561062eb37528
Daniele Murroni <empirico@gmail.com>
2011-07-13 21:08:48 +0200

    sistemata pagina music

--- music.php
+++ music.php
@@ -1,8 +1,8 @@
 <div class="page_layer">
     <div class="album_entry">
         <div class="cover">
-            <div width="250" style="background: url(images/ifwt.jpg)">
-                <img src="/images/cd_cover.png" width="250">
+            <div width="200" style="background: url(images/ifwt.jpg)">
+                <img src="/images/cd_cover.png" width="200">
             </div>
         </div>
         <div class="album_description">     
@@ -11,12 +11,6 @@
                 <param name="allowscriptaccess" value="always"></param> 
                 <embed allowscriptaccess="always" height="200" width="100%" src="http://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Fplaylists%2F226276&amp;show_comments=false&amp;auto_play=false&amp;show_playcount=false&amp;show_artwork=false" type="application/x-shockwave-flash" ></embed> 
             </object> 
-            <br/>
-            <p style="line-height: 32px;height:32px">buy it on 
-                    <a onclick="javascript:_gaq.push(['_trackPageview', '/download/infatwetrust']);" target="_blank" href="http://bonescomfort.bandcamp.com/album/in-fat-we-trust?action=buy&from=embed">
-                     <img title="bandcamp" src="/images/bandcamp_32.png" border="0" height="32" width="32" align="middle">
-                    </a>
-            </p>
         </div>
         <br style="clear:both">
     </div>
