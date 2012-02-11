4d694d8e11b44b05528d754670f075a58e47dc78
Daniele Murroni <empirico@gmail.com>
2012-02-11 20:59:42 +0100

    home admin

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -5,6 +5,11 @@ class Admin_IndexController extends Bones_Controller_Admin
 
     public function indexAction()
     {
+        $this->view->journals = JournalQuery::create()->limit(3)->orderById(Criteria::DESC)->find();
+        $this->view->posts = JournalPostQuery::create()->orderByCreated(Criteria::DESC)->limit(5)->find();
+        $this->view->galleries = GalleryQuery::create()->orderById(Criteria::DESC)->limit(3)->find();
+        $this->view->albums = AlbumQuery::create()->orderById(Criteria::DESC)->limit(3)->find();
+        $this->view->photos = PhotosQuery::create()->orderById(Criteria::DESC)->limit(3)->find();
     }
 
     public function errorAction(){
