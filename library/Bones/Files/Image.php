bbb9cdf1c8fc7fe9cdd849039fe1202a0304ae52
Daniele Murroni <daniele.murroni@triboo.it>
2012-03-01 09:31:27 +0100

    pulita la classe Bones_Files_Image

--- library/Bones/Files/Image.php
+++ library/Bones/Files/Image.php
@@ -53,52 +53,7 @@ class Bones_Files_Image extends Bones_Files_Base {
 			}
 			return $sourceImage;
     }
-    /*
-    public function resize($w = null, $h = null) {
-
-    	if (is_null($w) && is_null($h)) return;
-
-    	$sourcePath = $this->base_path . "/" . $this->getPath() . "/" . $this->getFilename();
-    	switch($this->getMimetype()) {
-
-			case self::MIME_JPG :
-			case self::MIME_JPEG :{
-				$sourceImage =imagecreatefromjpeg($sourcePath);
-			}
-			break;
-			case self::MIME_PNG: {
-				$sourceImage =imagecreatefrompng($sourcePath);
-			}
-			break;
-			default:
-				return;
-			break;
-		}
-
-		if ($sourceImage) {
-			$sourceX = imageSX($sourceImage);
-			$sourceY = imageSY($sourceImage);
-
-			if (isset ($w) AND !isset ($h)) {
-				// autocompute height if only width is set
-				$h = (100 / ($sourceX / $w)) * .01;
-				$h = @round ($sourceY * $h);
-			} elseif (isset ($h) AND !isset ($w)) {
-				// autocompute width if only height is set
-				$w = (100 / ($sourceY / $h)) * .01;
-				$w = @round ($sourceX * $w);
-			}
-
-			// Create the resized image destination
-			$resized = @ImageCreateTrueColor ($w, $h);
-			// Copy from image source, resize it, and paste to image destination
-			@ImageCopyResampled ($resized, $sourceImage, 0, 0, 0, 0, $w, $h, $sourceX, $sourceY);
-			// Output resized image
-			$resized_fileName = $w . "_" . $this->getFilename();
-			@ImageJPEG ($resized, $this->base_path . $this->id_to_path() . $resized_fileName);
-		}
-	}
-    */
+    
 	public function getHeight($width = '') {
 		$source = $this->getBlob($width);
 		if (is_null($source)) return "100%";
